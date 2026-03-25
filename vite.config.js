b0dd16731c92afaa752b069f280cdc112dbd938f
usamamanzoor991 <usamamanzoor991@gmail.com>
2026-03-25 09:28:50 +0500

    add(google analytics)

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,19 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import tailwindcss from '@tailwindcss/vite'
+import { VitePluginRadar } from 'vite-plugin-radar'
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react(),tailwindcss()],
+  plugins: [
+    react(),
+    tailwindcss(),
+    VitePluginRadar({
+      // Google Analytics 4
+      enableDev: false,
+      analytics: {
+        id: 'G-GGR9QHGFLK',  // Your GA4 Measurement ID
+      },
+    }),
+  ],
 })
